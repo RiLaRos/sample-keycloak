e431092d25836bfda9675af7fe116de081d8cad2
Ricardo La Rosa <rilaros@gmail.com>
2023-08-12 11:18:29 -0500

    feature | added keycloak integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+import { KeycloakProfile } from 'keycloak-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sample-keycloak';
+  public isLogged = false;
+  public userProfile: KeycloakProfile | null = null;
+
+  constructor(private readonly keycloak : KeycloakService){
+
+  }
+
+  public async ngOnInit() {
+    this.isLogged = await this.keycloak.isLoggedIn();
+    type userRoles = Array<{id:number, text:string}>;
+
+    if(this.isLogged){
+      this.userProfile = await this.keycloak.loadUserProfile();
+    }
+  }
+
+  public login(){
+    this.keycloak.login();
+  }
+
+  public logout() {
+    this.keycloak.logout();
+  }
 }
